02adddfd65812fd8bace18049adabf8bea60994b
Aidan Feldman <aidan.feldman@gmail.com>
2010-09-20 01:59:41 -0400

    added Static pages controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,8 @@ Flyspace::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  
+  root :to => "static#index"
 
   # See how all your routes lay out with "rake routes"
 
