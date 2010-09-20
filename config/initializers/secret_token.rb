f014eb4e8c4c89980659006f0fecf50c24de69eb
Aidan Feldman <aidan.feldman@gmail.com>
2010-09-19 23:06:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flyspace::Application.config.secret_token = '980febd3469e6b97c2db3217253b9e8e90f952fc0169445445c14b639019e819cbad48ea9443ed6f2e659458ff559a9332d72e5fb8fe24ca6fff8a64515c4bf9'
