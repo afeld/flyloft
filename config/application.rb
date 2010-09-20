4938613d4578047a0b3b64258e89dd2ae22232f8
Aidan Feldman <aidan.feldman@gmail.com>
2010-09-20 01:06:14 -0400

    removed HAML templating

--- config/application.rb
+++ config/application.rb
@@ -38,9 +38,5 @@ module Flyspace
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    
-    config.generators do |g|
-      g.template_engine :haml
-    end
   end
 end
