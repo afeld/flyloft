6f06ba38747b245a3f12143c78536c202f220e9e
Aidan Feldman <aidan.feldman@gmail.com>
2010-12-04 00:46:36 -0500

    reject empty auditions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function remove_fields(link) {
-  $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".fields").hide();
-}
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
+  var regexp = new RegExp("new_" + association, "g");
   $(link).parent().before(content.replace(regexp, new_id));
 }
+
+$(document).ready(function(){
+  $(".remove-field").live('click', function(e) {
+    $(this).prev("input[type=hidden]").val("1");
+    $(this).closest("fieldset").hide();
+  });
+});
