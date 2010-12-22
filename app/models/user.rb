1186d04a72250125c1fce636d131325b101d82e8
Aidan Feldman <aidan.feldman@gmail.com>
2010-12-22 02:54:23 -0600

    added CanCan with super user privileges

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,16 @@ class User < ActiveRecord::Base
   has_many :org_memberships, :dependent => :destroy
   has_many :memberships, :through => :org_memberships, :source => :org
   has_many :contact_person_for, :class_name => 'Org', :foreign_key => 'contact_person_id', :dependent => :nullify
+  has_and_belongs_to_many :roles
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
   
   def display_name
     [first_name, last_name].join(" ")
+  end
+
+  def role?(role)
+    return !!self.roles.find_by_name(role.to_s)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101203050904) do
+ActiveRecord::Schema.define(:version => 20101222084420) do
 
   create_table "auditions", :force => true do |t|
     t.integer  "gig_id"
@@ -68,6 +68,17 @@ ActiveRecord::Schema.define(:version => 20101203050904) do
     t.integer  "founding_year"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
   end
 
   create_table "users", :force => true do |t|
